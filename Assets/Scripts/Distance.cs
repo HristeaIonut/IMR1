5b39e92c5d3826eefbe9140a40ed4e6e47acf681
HristeaIonut <hristeaionut72@gmail.com>
2021-10-01 22:39:43 +0300

    add two prefabs which interact

---
+++ Assets/Scripts/Distance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour
+{
+    public GameObject unCactus, doiCactus;
+    public float distance;
+    public Animator animatorUnu, animatorDoi;
+    void Start(){ }
+
+    void Update() {
+        distance = Vector3.Distance(unCactus.transform.position, doiCactus.transform.position);
+
+        if (distance < .2) {
+            animatorUnu.SetTrigger("Fight");
+            animatorDoi.SetTrigger("Fight");
+        }
+        else {
+            animatorUnu.SetTrigger("Chill");
+            animatorDoi.SetTrigger("Chill");
+        }
+    }
+}
